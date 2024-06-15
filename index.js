341190cbd20ceb72ade5aaa66c9aa8a7a63a41ff
Ganesh aditya <42596043+Ganeshaditya@users.noreply.github.com>
2024-06-15 13:23:02 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -8,29 +8,51 @@ const io = socketIo(server);
 
 app.use(express.static('public'));
 
-const pollData = { option1: 0, option2: 0 };
+const performance = {
+    "0": { votes: 0, label: "Bad", color: randomRGB() },
+    "1": { votes: 0, label: "Not Bad", color: randomRGB() },
+    "2": { votes: 0, label: "Good", color: randomRGB() },
+    "3": { votes: 0, label: "Fine", color: randomRGB() },
+    "4": { votes: 0, label: "Perfect", color: randomRGB() }
+};
+
+// Generate a random RGB color
+function randomRGB() {
+    const r = () => Math.random() * 256 >> 0;
+    return `rgb(${r()}, ${r()}, ${r()})`;
+}
 const chatHistory = [];
 const users = {};
 
 io.on('connection', (socket) => {
     console.log('A user connected');
 
-    socket.emit('updatePoll', pollData);
+    socket.emit('updatePoll', performance);
     socket.emit('updateChat', chatHistory);
 
-    socket.on('vote', (option) => {
-        if (pollData[option] !== undefined) {
-            pollData[option]++;
-            io.emit('updatePoll', pollData);
+    // On new vote
+    socket.on("vote", (index) => {
+
+        // Increase the vote at index
+        if (performance[index]) {
+            performance[index].votes += 1;
         }
+
+        // Show the performance in the console for testing
+        console.log(performance);
+
+        // Tell everybody else about the new vote
+        io.emit("updatePoll", performance);
     });
 
+    // sending message
     socket.on('sendMessage', (message) => {
         const userMessage = `${users[socket.id]}: ${message}`;
         chatHistory.push(userMessage);
         io.emit('updateChat', chatHistory);
     });
 
+    // setting user name for basic authentication for editing the messages
     socket.on('setUsername', (username) => {
         users[socket.id] = username;
     });
@@ -43,7 +65,8 @@ io.on('connection', (socket) => {
         delete users[socket.id];
         console.log('User disconnected');
     });
-    
+
+    // edit message
     socket.on('editMessage', (index, newMessage) => {
         const [username] = chatHistory[index].split(': ');
         if (username === users[socket.id]) {
@@ -51,7 +74,8 @@ io.on('connection', (socket) => {
             io.emit('updateChat', chatHistory);
         }
     });
-    
+
+    // delete message
     socket.on('deleteMessage', (index) => {
         const [username] = chatHistory[index].split(': ');
         if (username === users[socket.id]) {
@@ -59,7 +83,7 @@ io.on('connection', (socket) => {
             io.emit('updateChat', chatHistory);
         }
     });
-    
+
 });
 
 const PORT = process.env.PORT || 3000;
